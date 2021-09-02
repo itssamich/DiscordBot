6d99d80b8b2ff31c1c33d4aa39c981f796727a36
Alex Klein <61808825+itssamich@users.noreply.github.com>
2021-09-02 05:32:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from web_server_alive import stay_running
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$test'):
+        await message.channel.send('Test!')
+
+stay_running()
+client.run(os.environ['Token'])
